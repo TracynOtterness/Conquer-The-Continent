aa0667f2f04098da399ffc7fb3c206f19661fa10
TracynOtterness <tracynot20@students.nsummit.org>
2018-03-28 08:38:32 -0600

    Add files via upload

--- Assets/scripts/Hexagon.cs
+++ Assets/scripts/Hexagon.cs
@@ -5,10 +5,12 @@ public class Hexagon : MonoBehaviour {
 	public Instantiator instantiator;
 	public int HexPosX;
 	public int HexPosY;
-    public int NationNum;
+    public int nationNum;
 	private float b = .2f;
 	public bool isLand = false;
+    public bool isCountry = false;
 	private Sprite[] spriteArray;
+    public GameObject[] adj = new GameObject[6];
 
 	void Awake () {
 		spriteArray = Resources.LoadAll<Sprite>("sprites");
@@ -21,10 +23,30 @@ public class Hexagon : MonoBehaviour {
 			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y + b, this.transform.position.z);
 		}
 	}
+    void Start(){
+
+            adj[0] = GameObject.Find("Hexagon " + HexPosX + " , " + (HexPosY + 1));
+            adj[3] = GameObject.Find("Hexagon " + HexPosX + " , " + (HexPosY - 1));
+             if (HexPosX % 2 == 0)
+        {
+            adj[1] = GameObject.Find("Hexagon " + (HexPosX + 1) + " , " + HexPosY);
+            adj[2] = GameObject.Find("Hexagon " + (HexPosX + 1) + " , " + (HexPosY - 1));
+            adj[4] = GameObject.Find("Hexagon " + (HexPosX - 1) + " , " + (HexPosY - 1));
+            adj[5] = GameObject.Find("Hexagon " + (HexPosX - 1) + " , " + HexPosY);
+        }
+        else
+        {
+            adj[1] = GameObject.Find("Hexagon " + (HexPosX + 1) + " , " + (HexPosY + 1));
+            adj[2] = GameObject.Find("Hexagon " + (HexPosX + 1) + " , " + HexPosY);
+            adj[4] = GameObject.Find("Hexagon " + (HexPosX - 1) + " , " + HexPosY);
+            adj[5] = GameObject.Find("Hexagon " + (HexPosX - 1) + " , " + (HexPosY + 1));
+        }
+        }
+
 	public void CreateLand() {
 		this.isLand = true;
-        NationNum = Random.Range(3,8);
-		this.GetComponent<SpriteRenderer> ().sprite = spriteArray [NationNum];
+        nationNum = Random.Range(3,8);
+		this.GetComponent<SpriteRenderer> ().sprite = spriteArray [nationNum];
 		instantiator.totalHexes++;
 }
 }
