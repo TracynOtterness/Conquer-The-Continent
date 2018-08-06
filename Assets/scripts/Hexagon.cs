519e831b8165f211d2e118714245c4f2e86cfffa
TracynOtterness <tracynot20@students.nsummit.org>
2018-08-06 12:54:47 -0600

    Update Hexagon.cs

--- Assets/scripts/Hexagon.cs
+++ Assets/scripts/Hexagon.cs
@@ -1,33 +1,40 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class Hexagon : MonoBehaviour {
-	public Instantiator instantiator;
 	public int HexPosX;
 	public int HexPosY;
     public int nationNum;
-	private float b = .2f;
+	private float b = .193f;
 	public bool isLand = false;
     public bool isCountry = false;
 	private Sprite[] spriteArray;
     public GameObject[] adj = new GameObject[6];
+    public int guardedBy = 0;
+    public bool hasGuard;
+    public bool hasCapital;
+    public bool hasGrave;
+    public GameObject village;
+    public GameObject grave;
+    public Sprite[]capitalSpriteArray = new Sprite[2];
+    public Person guard; 
 
 	void Awake () {
-		spriteArray = Resources.LoadAll<Sprite>("sprites");
-		instantiator = GameObject.FindObjectOfType<Instantiator> ();
-		HexPosX = instantiator.HexPosX;
-		HexPosY = instantiator.HexPosY;
+		spriteArray = Resources.LoadAll<Sprite>("Hexes");
+		HexPosX = Instantiator.HexPosX;
+		HexPosY = Instantiator.HexPosY;
 		string HexString = "Hexagon " + HexPosX + " , " + HexPosY;
 		this.name = HexString;
 		if (HexPosX % 2 == 1) {
 			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y + b, this.transform.position.z);
 		}
+        EditFlag();
 	}
     void Start(){
 
             adj[0] = GameObject.Find("Hexagon " + HexPosX + " , " + (HexPosY + 1));
             adj[3] = GameObject.Find("Hexagon " + HexPosX + " , " + (HexPosY - 1));
-             if (HexPosX % 2 == 0)
+        if (HexPosX % 2 == 0)
         {
             adj[1] = GameObject.Find("Hexagon " + (HexPosX + 1) + " , " + HexPosY);
             adj[2] = GameObject.Find("Hexagon " + (HexPosX + 1) + " , " + (HexPosY - 1));
@@ -47,6 +54,25 @@ public class Hexagon : MonoBehaviour {
 		this.isLand = true;
         nationNum = Random.Range(3,8);
 		this.GetComponent<SpriteRenderer> ().sprite = spriteArray [nationNum];
-		instantiator.totalHexes++;
+		Instantiator.totalHexes++;
 }
+    public void ChangeSprite(int a){
+        this.GetComponent<SpriteRenderer>().sprite = spriteArray[a];
+    }
+    public void AddCapital(){
+        hasCapital = true;
+        village = this.gameObject.transform.GetChild(0).gameObject;
+        guardedBy = 1;
+    }
+    public void RemoveCapital(){
+        hasCapital = false;
+        Destroy(village);
+    }
+    public void EditFlag(){
+        if (village == null) return;
+        if (transform.parent.GetComponent<Country>().food >= 10){
+            village.GetComponent<SpriteRenderer>().sprite = capitalSpriteArray[0];
+        }
+        else village.GetComponent<SpriteRenderer>().sprite = capitalSpriteArray[1];
+    }
 }
