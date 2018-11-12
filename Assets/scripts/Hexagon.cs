4df55b5a59c5dc50adecb0c7c3eeeff94165e2df
TracynOtterness <tracynot20@students.nsummit.org>
2018-11-12 08:45:53 -0700

    Update Hexagon.cs

--- Assets/scripts/Hexagon.cs
+++ Assets/scripts/Hexagon.cs
@@ -5,24 +5,34 @@ public class Hexagon : MonoBehaviour {
 	public int HexPosX;
 	public int HexPosY;
     public int nationNum;
-	private float b = .193f;
-	public bool isLand = false;
-    public bool isCountry = false;
+    private float b = .279f;
 	private Sprite[] spriteArray;
     public GameObject[] adj = new GameObject[6];
     public int guardedBy = 0;
+    public bool isLand = false;
+    public bool isCountry = false;
     public bool hasGuard;
     public bool hasCapital;
     public bool hasGrave;
+    public bool hasCastle;
+    public bool hasHarbor;
+    public bool hasShip;
+    bool maskToggled;
     public GameObject village;
+    public GameObject shipCapital;
+    public GameObject castle;
+    public GameObject harbor;
     public GameObject grave;
     public Sprite[]capitalSpriteArray = new Sprite[2];
-    public Person guard; 
+    public Person guard;
+    public Ship ship;
+    public int distanceFromStartingPoint;
 
 	void Awake () {
 		spriteArray = Resources.LoadAll<Sprite>("Hexes");
 		HexPosX = Instantiator.HexPosX;
 		HexPosY = Instantiator.HexPosY;
+        distanceFromStartingPoint = -1;
 		string HexString = "Hexagon " + HexPosX + " , " + HexPosY;
 		this.name = HexString;
 		if (HexPosX % 2 == 1) {
@@ -31,7 +41,7 @@ public class Hexagon : MonoBehaviour {
         EditFlag();
 	}
     void Start(){
-
+        
             adj[0] = GameObject.Find("Hexagon " + HexPosX + " , " + (HexPosY + 1));
             adj[3] = GameObject.Find("Hexagon " + HexPosX + " , " + (HexPosY - 1));
         if (HexPosX % 2 == 0)
@@ -52,21 +62,28 @@ public class Hexagon : MonoBehaviour {
 
 	public void CreateLand() {
 		this.isLand = true;
-        nationNum = Random.Range(3,8);
+        nationNum = Random.Range(1 , UIManager.playerCount + 1);
 		this.GetComponent<SpriteRenderer> ().sprite = spriteArray [nationNum];
 		Instantiator.totalHexes++;
 }
     public void ChangeSprite(int a){
         this.GetComponent<SpriteRenderer>().sprite = spriteArray[a];
     }
-    public void AddCapital(){
-        hasCapital = true;
-        village = this.gameObject.transform.GetChild(0).gameObject;
-        guardedBy = 1;
+    public void AddCapital(){ 
+            hasCapital = true;
+            foreach(Transform t in this.transform){
+            if(t.tag == "capital"){
+                village = t.gameObject;
+            }
+            }
+            print(this + " is now a capital. Village is " + village);
+            guardedBy = 1; 
     }
     public void RemoveCapital(){
         hasCapital = false;
+        print(village);
         Destroy(village);
+        print("you destroyed the capital!");
     }
     public void EditFlag(){
         if (village == null) return;
@@ -74,5 +91,34 @@ public class Hexagon : MonoBehaviour {
             village.GetComponent<SpriteRenderer>().sprite = capitalSpriteArray[0];
         }
         else village.GetComponent<SpriteRenderer>().sprite = capitalSpriteArray[1];
+    }
+    public void ToggleMask(){
+        if (maskToggled){
+            transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+            maskToggled = false;
+        }
+        else{
+            transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+            maskToggled = true;
+        }
+    }
+    public void ToggleShipColor(int shipNationNum){
+        print("ToggleShipColor");
+        if (transform.GetChild(1).GetComponent<SpriteRenderer>().enabled == true){
+            transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = false;
+        }
+        else{
+            transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = true;
+            switch(shipNationNum){
+                case 1: transform.GetChild(1).GetComponent<SpriteRenderer>().color = new Color32(6, 240, 255, 255); break;
+                case 2: transform.GetChild(1).GetComponent<SpriteRenderer>().color = new Color32(255, 0, 255, 255); break;
+                case 3: transform.GetChild(1).GetComponent<SpriteRenderer>().color = new Color32(0, 255, 8, 255); break;
+                case 4: transform.GetChild(1).GetComponent<SpriteRenderer>().color = new Color32(255, 255, 0, 255); break;
+                case 5: transform.GetChild(1).GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 255); break;
+                case 6: transform.GetChild(1).GetComponent<SpriteRenderer>().color = new Color32(21, 134, 24, 255); break;
+                case 7: transform.GetChild(1).GetComponent<SpriteRenderer>().color = new Color32(255, 174, 0, 255); break;
+                case 8: transform.GetChild(1).GetComponent<SpriteRenderer>().color = new Color32(121, 22, 177, 255); break;
+            }
+        }
     }
 }
