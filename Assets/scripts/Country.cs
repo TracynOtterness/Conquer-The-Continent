0a10c0e0ee72fa4640ab5b8ba2297948c8a5a44f
TracynOtterness <tracynot20@students.nsummit.org>
2019-04-18 09:06:40 -0600

    Update Country.cs

--- Assets/scripts/Country.cs
+++ Assets/scripts/Country.cs
@@ -79,6 +79,7 @@ public class Country : MonoBehaviour
             }
             else if (child.tag.Equals("person"))
             {
+                //print(child);
                 people.Add(child.GetComponent<Person>());
             }
             else if (child.tag.Equals("ship")){
@@ -102,7 +103,7 @@ public class Country : MonoBehaviour
         }
     }
     void RemoveFromCountryLists(){
-        print(this);
+        //print(this);
         if (!Instantiator.spawningCountries){ Starve();}
         foreach(Hexagon h in hexagons){
             h.transform.SetParent(Instantiator.nonHexFolder.transform);
@@ -122,17 +123,17 @@ public class Country : MonoBehaviour
         food = hexagons.Count * 5;
     }
     public void GiveRoundFood(){
-        if (isInvasionCountry) { print("Hello"); }
+       // if (isInvasionCountry) { print("Hello"); }
         foreach (Hexagon h in hexagons)
         {
             if (h.hasGrave == false) food++;
         }
         try
         {
-            print(this + " has " + people.Count + " people");
+            //print(this + " has " + people.Count + " people");
             foreach (Person p in people)
             {
-                print(p);
+                //print(p);
                 if (!p.isSailor)
                 {
                     food -= 2 * (int)Math.Pow(3, (p.tier - 1));
@@ -153,25 +154,33 @@ public class Country : MonoBehaviour
                 food = hexagons.Count;
             }
         }
-        catch (Exception) { print("test"); }
+        catch (Exception) { print("Caught Exception"); }
     }
     public void UpdateGuard(){
+        print("UpdateGuard for " + this);
+        foreach(Hexagon h in hexagons)
+        {
+            h.guardedBy = 0;
+        }
         foreach (Hexagon h in hexagons){
-            if (h.hasGuard == false && h.hasCastle == false && h.hasHarbor == false && h.hasCapital == false) h.guardedBy = 0;
-            if (h.hasCapital || h.hasHarbor)
+            if (h.hasCapital || h.hasHarbor && h.guardedBy < 1)
             {
                 h.guardedBy = 1;
             }
-            if(h.hasCastle){
-                if(h.guardedBy < 2){
-                    h.guardedBy = 2;  
-                }
+            if(h.hasCastle && h.guardedBy < 2){
+                print("castle located");
+                h.guardedBy = 2;  
                 foreach (GameObject g in h.adj)
                 {
                     if (g.transform.parent == h.transform.parent)
                     {
+                        print("1");
                         Hexagon adjHexScript = g.GetComponent<Hexagon>();
-                        if (adjHexScript.guardedBy < 2) adjHexScript.guardedBy = 2;
+                        print(adjHexScript);
+                        if (adjHexScript.guardedBy < 2) {
+                            print("2");
+                            adjHexScript.guardedBy = 2; 
+                        }
                     }
                 }
             }
@@ -192,6 +201,7 @@ public class Country : MonoBehaviour
     }
     public void SetCapital(Hexagon chosenHexagon = null)
     {
+        print("SetCapital");
         GameObject capitalObject;
         if(chosenHexagon == null){
             //this could probably be a little bit optimized?
@@ -245,15 +255,25 @@ public class Country : MonoBehaviour
             }
         }
         else{
+            if (isInvasionCountry)
+            {
+                this.capital.shipCapital.GetComponent<SpriteRenderer>().enabled = true;
+                this.capital.shipCapital.GetComponent<Ship>().moored = false;
+                foreach (Person p in this.capital.shipCapital.GetComponent<Ship>().sailors)
+                {
+                    p.transform.SetParent(this.capital.shipCapital.transform);
+                    p.hexCountry = null;
+                }
+                this.capital.shipCapital = null;
+                this.isInvasionCountry = false;
+            }
             capital.RemoveCapital();
             capital = chosenHexagon;
             capitalObject = (GameObject)Instantiate(Resources.Load("Capital"), new Vector3(capital.transform.position.x, capital.transform.position.y, 0f), Quaternion.identity);
             capitalObject.transform.SetParent(capital.transform);
             capital.AddCapital();
             capital.EditFlag();
-            if(isInvasionCountry){
-                
-            }
+
         }
      }
     public void CheckCountrySplit()
@@ -293,6 +313,7 @@ public class Country : MonoBehaviour
                 }
                 else
                 {
+                    print("Non capital run");
                     switch (runsCount)
                     {
                         case 0: newCountryHexes1 = new List<Hexagon>(currentRunHexes); break;
@@ -343,7 +364,7 @@ public class Country : MonoBehaviour
             if (activeHexes[i].hasCapital)
             {
                 capitalRun = true;
-
+                print("This run is a capital run");
             }
             activeHexes.Remove(activeHexes[i]);
             runCount++;
@@ -352,32 +373,111 @@ public class Country : MonoBehaviour
     void UpdateLegacyCountry()
     {
         hexagons = new List<Hexagon>(legacyHexes);
-        UpdateArray();
+        print("LegacyHexes:");
+        foreach(Hexagon h in legacyHexes)
+        {
+            print(h);
+        }
         if (specialTripleSplit) {
             print("specialTripleSplit");
             if (hexagons.Count < 2)
             {
                 foreach (Hexagon h in hexagons)
                 {
+                    print(h + "is getting sent to NonHexFolder");
                     h.isCountry = false;
                     h.transform.SetParent(Instantiator.nonHexFolder.transform);
+                    if (h.hasGuard) //starve the single guy off
+                    {
+                        Person singleHexGuy = h.guard;
+                        GameObject grave = (GameObject)Instantiate(Resources.Load("Grave"), new Vector3(singleHexGuy.transform.position.x, singleHexGuy.transform.position.y, 0), Quaternion.identity);
+                        h.hasGrave = true;
+                        h.grave = grave;
+                        grave.transform.SetParent(h.transform);
+                        grave.name = "grave " + singleHexGuy;
+                        h.hasGuard = false;
+                        UIManager.allPeople.Remove(singleHexGuy);
+                        Destroy(singleHexGuy.gameObject);
+                        UIManager.SpawnRedDots();
+                    }
+                    if (h.hasCapital)
+                    {
+                        h.RemoveCapital();
+                    }
+                    h.guardedBy = 0;
                 }
-                RemoveFromCountryLists();
+                print("removeFromCountryLists");
+                Invoke("RemoveFromCountryLists", .1f);
             }
             food = 0;
         }
+        else if (hexagons.Count < 2)
+        {
+            foreach (Hexagon h in hexagons)
+            {
+                print(h + "is getting sent to NonHexFolder");
+                h.isCountry = false;
+                h.transform.SetParent(Instantiator.nonHexFolder.transform);
+                if (h.hasGuard) //starve the single guy off
+                {
+                    Person singleHexGuy = h.guard;
+                    GameObject grave = (GameObject)Instantiate(Resources.Load("Grave"), new Vector3(singleHexGuy.transform.position.x, singleHexGuy.transform.position.y, 0), Quaternion.identity);
+                    h.hasGrave = true;
+                    h.grave = grave;
+                    grave.transform.SetParent(h.transform);
+                    grave.name = "grave " + singleHexGuy;
+                    h.hasGuard = false;
+                    UIManager.allPeople.Remove(singleHexGuy);
+                    Destroy(singleHexGuy.gameObject);
+                    UIManager.SpawnRedDots();
+                }
+                if (h.hasCapital)
+                {
+                    h.RemoveCapital();
+                }
+                h.guardedBy = 0;
+            }
+        }
+        UpdateArray();
+        UpdateGuard();
     }
     void CreateCountryOne()
     {
-        if (newCountryHexes1.Count < 2){
+        print("createCountryOne");
+        if (newCountryHexes1.Count < 2){ //single hexagon cut off
             foreach(Hexagon h in newCountryHexes1){
+                print(h + "is getting sent to NonHexFolder");
                 h.isCountry = false;
                 h.transform.SetParent(Instantiator.nonHexFolder.transform);
+                if (h.hasGuard) //starve the single guy off
+                {
+                    Person singleHexGuy = h.guard;
+                    GameObject grave = (GameObject)Instantiate(Resources.Load("Grave"), new Vector3(singleHexGuy.transform.position.x, singleHexGuy.transform.position.y, 0), Quaternion.identity);
+                    h.hasGrave = true;
+                    h.grave = grave;
+                    grave.transform.SetParent(h.transform);
+                    grave.name = "grave " + singleHexGuy;
+                    h.hasGuard = false;
+                    UIManager.allPeople.Remove(singleHexGuy);
+                    Destroy(singleHexGuy.gameObject);
+                    UIManager.SpawnRedDots();
+                }
+                if (h.hasCapital)
+                {
+                    h.RemoveCapital();
+                }
+                h.guardedBy = 0;
             }
             return;
         }
         GameObject country = (GameObject)Instantiate(Resources.Load("Country"), new Vector3(0, 0, 0), Quaternion.identity);
         Country c = country.GetComponent<Country>();
+        print(country);
+        print("NewHexes1:");
+        foreach(Hexagon h in newCountryHexes1)
+        {
+            print(h);
+        }
         c.hexagons = new List<Hexagon>(newCountryHexes1);
         Instantiator.countryscripts.Add(c);
         foreach (Hexagon h in c.hexagons)
@@ -396,8 +496,10 @@ public class Country : MonoBehaviour
             p.transform.SetParent(h.transform.parent);
         }
         c.UpdateArray();
+        c.UpdateGuard();
         c.SetCapital();
     }
+
     void CreateCountryTwo()
     {
         print("CreateCountryTwo");
@@ -407,6 +509,24 @@ public class Country : MonoBehaviour
             {
                 h.isCountry = false;
                 h.transform.SetParent(Instantiator.nonHexFolder.transform);
+                if (h.hasGuard) //starve the single guy off
+                {
+                    Person singleHexGuy = h.guard;
+                    GameObject grave = (GameObject)Instantiate(Resources.Load("Grave"), new Vector3(singleHexGuy.transform.position.x, singleHexGuy.transform.position.y, 0), Quaternion.identity);
+                    h.hasGrave = true;
+                    h.grave = grave;
+                    grave.transform.SetParent(h.transform);
+                    grave.name = "grave " + singleHexGuy;
+                    h.hasGuard = false;
+                    UIManager.allPeople.Remove(singleHexGuy);
+                    Destroy(singleHexGuy.gameObject);
+                    UIManager.SpawnRedDots();
+                }
+                if (h.hasCapital)
+                {
+                    h.RemoveCapital();
+                }
+                h.guardedBy = 0;
             }
             return;
         }
@@ -432,6 +552,7 @@ public class Country : MonoBehaviour
             p.transform.SetParent(h.transform.parent);
         }
         c.UpdateArray();
+        c.UpdateGuard();//just put this here
         c.SetCapital();
     }
 
@@ -453,8 +574,8 @@ public class Country : MonoBehaviour
         }
         UIManager.SpawnRedDots();
         people.Clear();
-        UpdateArray();
-        UpdateGuard();
+        Invoke("UpdateArray", .1f);
+        Invoke("UpdateGuard", .2f);
     }
        public int GiveIncome(){
         int i = 0;
