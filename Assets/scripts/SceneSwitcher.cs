f56e843e27b75df90a7bea62c918acbdaf2e111b
TracynOtterness <tracynot20@students.nsummit.org>
2019-04-18 09:16:33 -0600

    Update SceneSwitcher.cs

--- Assets/scripts/SceneSwitcher.cs
+++ Assets/scripts/SceneSwitcher.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour {
@@ -9,28 +9,6 @@ public class SceneSwitcher : MonoBehaviour {
     string scene;
 
     public void SwitchScene(string s){
-        if (s == "Scene1"){
-            playGame = true;
-            audioSource = GameObject.FindWithTag("Audio").GetComponent<AudioSource>();
-            print(audioSource);
-            scene = s;
-        }
-        else{
-            SceneManager.LoadScene(s);
-        }
+        SceneManager.LoadScene(s);
     }
-
-	private void Update()
-	{
-        if (playGame){
-            if (audioSource.volume - .5f * Time.deltaTime <= 0){
-                audioSource.volume = 0;
-                audioSource.clip = audioClip;
-                SceneManager.LoadScene(scene);
-            }
-            else{
-                audioSource.volume -= .5f * Time.deltaTime;
-            }
-        }
-	}
 }
